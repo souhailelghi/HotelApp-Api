8869ecfcd6038b8d0f29d31e8076d30374b1dd05
souhail elghiouane <149603630+souhailelghi@users.noreply.github.com>
2026-05-10 17:04:32 +0100

    add new class

---
+++ HotelApp-Api/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelApp_Api.Models
+{
+    public class User
+    {
+        [Key]
+        public int IdUser { get; set; }
+
+        [MaxLength(50)]
+        public string Nom { get; set; }
+
+        [MaxLength(50)]
+        public string Prenom { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(255)]
+        public string MotDePasse { get; set; }
+
+        [MaxLength(20)]
+        public string Telephone { get; set; }
+    }
+}
